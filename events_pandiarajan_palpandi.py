afc83752d3f2f10fecb4c68d576f068fc5fbaa74
pandiarajan-palpandi <Pandiarajan.Palpandi@lincolnuni.ac.nz>
2025-03-21 01:31:38 +1300

    Task 4. Buy Tickets

--- events_pandiarajan_palpandi.py
+++ events_pandiarajan_palpandi.py
@@ -19,12 +19,24 @@ CUSTOMERS = 'customers'
 DATE_FORMAT = '%d/%m/%Y'
 EMAIL_PATTERN = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
+#properties for collecting user inputs
+user_inputs = {
+    'customer_id': 0,
+    'event_name': '',
+    'tickets_to_buy': 0,
+    'option': '1',
+    'tickets_to_buy': 0
+}
+
+
+
 
 def list_all_customers():
     """
     Lists customer details.
     This is an example of how to produce basic output."""
     format_str = "{: <5} {: <15} {: <15} {: <14} {: <20}"  
+    print('\nEXISTING CUSTOMER INFORMATION\n')
     print('----------------------------------------------------------------------------------------')          # Use the same format_str for column headers and rows to ensure consistent spacing. 
     display_formatted_row(["ID","First Name","Family Name","Birth Date","e-Mail"],format_str)
     print('----------------------------------------------------------------------------------------')
@@ -82,15 +94,13 @@ def list_customers_and_tickets():
             print('{: >50}'.format('NO EVENTS BOOKED'))
         print('---------------------------------------------------------------------------------------\n')
 
-        
-    input("\nPress Enter to continue.")
-
 def list_event_details():
     """
     List the events, show all details except Customers who have purchased tickets."""
 
     event_name_list = []
     format_str = "{: <30} {: <15} {: <15} {: <10} {: <10}"
+    print('\nEVENT DETAILS')
     print('---------------------------------------------------------------------------------------')
     display_formatted_row(["Event Name","Age Restriction","Event Date","Capacity","Tickets Sold"],format_str)     # Use the display_formatted_row() function to display the column headers with consistent spacing
     print('---------------------------------------------------------------------------------------')
@@ -106,28 +116,17 @@ def list_event_details():
         details = events[event_name]
         display_formatted_row([event_name, details[AGE_LIMIT], details[EVENT_DATE].strftime("%d %b %Y"), details[CAPACITY], details[TICKETS_SOLD]], format_str)
     print('---------------------------------------------------------------------------------------')
-    
-    input("\nPress Enter to continue.")
 
 def buy_tickets():
     """
     Choose a customer, then a future event, the purchase can only proceed if they meet the minimum age requirement and tickets are available """
-    valid_id = False
-    print('\nEXISTING CUSTOMER INFORMATION\n')
-    list_all_customers()
-    customers_ids = list(zip(*customers))[0]
-    while valid_id == False:
-        customer_id = input('\nPlease provide the customer\'s id that you are buying tickets for: ')
-        try: 
-            customers_ids.index(customer_id)
-            valid_id = True
-        except:
-            print('Invalid input, Please try again!')
-            valid_id = False
-
-
-
-
+    while user_inputs["option"] == '1':
+        get_customer_id()
+        get_event_name()
+        get_tickets_to_buy()
+        book_ticket_for_customer()
+        reset_user_inputs()
+        get_options()
 
 
 def add_new_customer():
@@ -188,7 +187,7 @@ def list_future_available_events():
     """
     foundFutureEvent = False
     format_str = "{: <30} {: <15} {: <15} {: <10} {: <12} {: <10}"
-    print('\nFUTURE EVENT DETAILS\n')
+    print('\nFUTURE EVENT DETAILS')
     print('---------------------------------------------------------------------------------------------------------')
     display_formatted_row(["Event Name","Age Restriction","Event Date","Capacity","Tickets Sold", "Avaiable Tickets"],format_str)     # Use the display_formatted_row() function to display the column headers with consistent spacing
     print('---------------------------------------------------------------------------------------------------------')
@@ -202,8 +201,130 @@ def list_future_available_events():
             foundFutureEvent = True
     if foundFutureEvent == False:
         print('{: >70}'.format('NO FUTURE EVENTS WITH UNSOLD TICKETS FOUND'))
-    print('---------------------------------------------------------------------------------------------------------')      
-    input("\nPress Enter to continue.")
+    print('---------------------------------------------------------------------------------------------------------')
+    return foundFutureEvent     
+
+def event_exists():
+    return events.get(user_inputs["event_name"]) != None
+
+def is_customer_allowed():
+    allowed = False
+    try:
+        customers_ids = list(zip(*customers))[0]
+        customer = customers[customers_ids.index(int(user_inputs["customer_id"]))]
+        birth = customer[3]
+        event_age_limit = events[user_inputs["event_name"]][AGE_LIMIT]
+        if event_age_limit <= 0:
+            allowed = True
+            return allowed
+        today = date.today()
+        olderThan = date(today.year - event_age_limit, today.month, today.day)
+        if birth >=  olderThan:
+            allowed = False
+        return allowed
+    except:
+        print('\nException Occured! Validation Failed.')
+        return allowed
+
+def get_options():
+    valid_response = False
+    options = ['1','2']
+    while valid_response == False:
+        print("\n==== SELECT OPTIONS FROM BELOW ===")
+        print(" 1 - Try Again")
+        print(" 2 - Return to Main Menu")
+        selection =  input("\nPlease enter menu choice: ").strip().upper()
+        if options.count(selection) > 0:
+            valid_response = True
+            user_inputs["option"] = selection
+        else:
+            valid_response = False
+            print('\nInvalid Selection! PLease provde valid option from the list.')
+
+def reset_option():
+    user_inputs["option"] == '1'
+
+def reset_user_inputs():
+    user_inputs["customer_id"] = 0
+    user_inputs["event_name"] = ''
+    user_inputs["option"] = '1'
+    user_inputs["tickets_to_buy"] = 0
+
+def get_customer_id():
+    while user_inputs["option"] == '1':
+        list_all_customers()
+        customers_ids = list(zip(*customers))[0]
+        customer_id = input('\nPlease provide the customer\'s id that you are buying tickets for: ').strip()
+        if customer_id.isnumeric():
+            customer_id_num = int(customer_id)
+            if customers_ids.count(customer_id_num) > 0:
+                user_inputs["customer_id"] = customer_id_num
+                reset_option()
+                break
+            else:
+                print('\nCutomer not found! Please provide a valid ID number.')
+                get_options()
+        else:
+            print('\nInvalid Input! Please provide a valid ID number.')
+            get_options()
+        
+
+def get_event_name():
+    while user_inputs["option"] == '1':
+        list_future_available_events()
+        user_inputs["event_name"] = input('\nPlease enter the event name to buy ticket: ').strip()
+        if event_exists():
+            if is_customer_allowed():
+                break
+            else:
+                print('\nCustomer does not meet the age criteria! Hence can not book the event.')
+                get_options()
+        else:
+            print('\nEvent not found! Please provide a valid event name.')
+            get_options()
+
+
+def get_tickets_to_buy():
+    while user_inputs["option"] == '1':
+        tickets_to_buy = input(f'\nPlease enter the number of ticket to buy (Available Tickets - {events[user_inputs["event_name"]][CAPACITY] - events[user_inputs["event_name"]][TICKETS_SOLD]}): ').strip()
+        if tickets_to_buy.isnumeric():
+            user_inputs["tickets_to_buy"] = int(tickets_to_buy)
+            if int(events[user_inputs["event_name"]][CAPACITY]) - int(events[user_inputs["event_name"]][TICKETS_SOLD]) >= user_inputs["tickets_to_buy"]:
+                break
+            else:
+                print('\nTickets Unavailable! Please provide a valid ticket count.')
+                get_options()
+        else:
+            print('\nInvalid Number! Please provide a valid number.')
+            get_options()
+
+
+def book_ticket_for_customer():
+    if user_inputs["option"] == '1':
+        extra_ticket = False
+        details = events[user_inputs["event_name"]]
+        customer_and_tickets = details['customers']
+        if len(customer_and_tickets) == 0:
+            update_customer_booking(customer_and_tickets, user_inputs["tickets_to_buy"])
+            return None
+        for ct in customer_and_tickets:
+            if ct[0] == user_inputs["customer_id"]:
+                #existing customer ticket found and hence increasing the ticket count by one
+                total_tickets_bought = int(ct[1]) +  user_inputs["tickets_to_buy"]
+                customer_and_tickets.remove(ct)
+                update_customer_booking(customer_and_tickets, total_tickets_bought)
+                extra_ticket = True
+                break
+        if extra_ticket == False:
+            # No existing tickets found for the customer and hence appending new set
+            update_customer_booking(customer_and_tickets, user_inputs["tickets_to_buy"])
+        print('\nTickets booked successfully!')
+
+def update_customer_booking(customer_and_tickets, updated_count):
+    customer_and_tickets.append((user_inputs["customer_id"], updated_count))
+    events[user_inputs["event_name"]]['tickets_sold'] = events[user_inputs["event_name"]]['tickets_sold'] +  user_inputs["tickets_to_buy"]
+
+
 
 def disp_menu():
     """
@@ -230,14 +351,18 @@ while response != "X":
     response = input("Please enter menu choice: ").upper()
     if response == "1":
         list_all_customers()
+        input("\nPress Enter to continue.")
     elif response == "2":
         list_customers_and_tickets()
+        input("\nPress Enter to continue.")
     elif response == "3":
         list_event_details()
+        input("\nPress Enter to continue.")
     elif response == "4":
         buy_tickets()
     elif response == "5":
         list_future_available_events()
+        input("\nPress Enter to continue.")
     elif response == "6":
         add_new_customer()
     elif response != "X":
